dd325aed336c8e707c36f8e85b7e7dbbe90d806f
Carina Ullman <carinalullman@gmail.com>
2020-03-08 12:04:52 -0600

    Spotify Component for Bootspace

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "143d5c9bf98c40089e52f3d70702fe9d";
+export const redirectUri = "https://damp-ridge-38701.herokuapp.com/";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
